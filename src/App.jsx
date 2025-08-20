ee711b6ccff2b68e172e1096cf65745b0d5d49c4
qtr-deagle <cjalamares05@gmail.com>
2025-08-20 21:43:03 +0800

    Initial push

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+const App = () => {
+  const name = 'CJ'
+  const x = 10;
+  const y = 20;
+  const names = ['CJ', 'John', 'Doe'];
+
+  return (
+    <>
+      <div className='text-5xl'>App</div>
+      <p>Hello {name}</p>
+      <p>
+        The sum of {x} and {y} is {x + y}
+      </p>
+      <ul>
+        {names.map((name, index) => (
+          <li key={index}>{name}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default App
