26e9de53582d7f37c38674852c9f677629d2092e
qtr-deagle <cjalamares05@gmail.com>
2025-08-24 18:24:43 +0800

    Updated functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import HomeCards from "./components/HomeCards";
-import JobListing from "./components/JobListings";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider
+} from 'react-router-dom'
+import MainLayout from './layouts/MainLayout';
+import HomePage from './pages/HomePage'
+import JobsPage from './pages/JobsPage';
+import NotFoundPage from './pages/NotFoundPage';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobsPage />} />
+      <Route path='*' element={<NotFoundPage />} />
+    </Route>
+  )
+);
 
 const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <HomeCards />
-      <JobListing />
-
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a>
-      </section>
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App
--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,18 +1,18 @@
 import Jobs from '../jobs.json';
 import JobListing from './JobListing';
 
-const JobListings = () => {
-    console.log(Jobs);
+const JobListings = ({ isHome = false }) => {
+    const JobListings = isHome ? Jobs.slice(0, 3) : Jobs;
 
     return (
         <section className="bg-blue-50 px-4 py-10">
             <div className="container-xl lg:container m-auto">
                 <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-                    Browse Jobs
+                    {isHome ? 'Recent Jobs' : 'Browse Jobs'}
                 </h2>
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                    {Jobs.map((job) => (
-                        <JobListing key={job.id} job={ job } />
+                    {JobListings.map((job) => (
+                        <JobListing key={job.id} job={job} />
                     ))}
 
                 </div>
