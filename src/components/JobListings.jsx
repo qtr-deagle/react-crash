d777b7fb2605a4ade3c1f7d1c361022304a68979
qtr-deagle <cjalamares05@gmail.com>
2025-08-25 16:17:02 +0800

    Add JSON Server, data fetching, spinner, conditional fetch, proxy setup

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,8 +1,29 @@
-import Jobs from '../jobs.json';
+import { useState, useEffect } from 'react'
 import JobListing from './JobListing';
+import Spinner from './Spinner';
 
 const JobListings = ({ isHome = false }) => {
-    const JobListings = isHome ? Jobs.slice(0, 3) : Jobs;
+    const [jobs, setJobs] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchJobs = async () => {
+            const apiUrl = isHome
+                ? '/api/jobs?_limit=3'
+                : "/api/jobs";
+            try {
+                const res = await fetch(apiUrl);
+                const data = await res.json();
+                setJobs(data);
+            } catch (error) {
+                console.error('Error fetching data', error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchJobs();
+    }, []);
 
     return (
         <section className="bg-blue-50 px-4 py-10">
@@ -10,14 +31,19 @@ const JobListings = ({ isHome = false }) => {
                 <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
                     {isHome ? 'Recent Jobs' : 'Browse Jobs'}
                 </h2>
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                    {JobListings.map((job) => (
-                        <JobListing key={job.id} job={job} />
-                    ))}
 
-                </div>
+                {loading ? (
+                    <Spinner loading={loading} />
+                ) : (
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                        {jobs.map((job) => (
+                            <JobListing key={job.id} job={job} />
+                        ))}
+
+                    </div>
+                )}
             </div>
-        </section>
+        </section >
     )
 }
 
